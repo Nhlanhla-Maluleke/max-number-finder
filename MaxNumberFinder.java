5554ab4034820df436e15c36dd62a6ef33431a0b
Nhlanhla Maluleke <mllnhl031@myuct.ac.za>
2025-12-12 12:29:52 +0200

    Added the method that calculates the maximum of the numbers given by user

---
+++ MaxNumberFinder.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**Side project of finding the maximum number of the given numbers by user
+ * Nhlanhla Maluleke
+ * 2025/12/10
+ */
+public class MaxNumberFinder {
+    static ArrayList<Integer> list;
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers do you want to put in?");
+        int k = input.nextInt();
+        int idx = 0; 
+        list = new ArrayList<Integer>();
+       do {
+            System.out.println("Enter the numbers:");
+            int i = input.nextInt();
+            list.add(i);
+            idx++;
+        }while(k>idx);
+        System.out.println("Maximum number: "+max(list));
+        
+    }
+//The method that calculates the maximum number between any given numbers
+    static int max(ArrayList<Integer> list){
+        int maxValue = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+          if(list.get(i)>maxValue){
+            maxValue = list.get(i);
+          }
+        }
+        return maxValue;
+
+    }
+
+}
+
